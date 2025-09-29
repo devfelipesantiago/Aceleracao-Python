36daf45ddc1f0fdfc6e3d222b2109b8b5d725821
Felipe Santiago <devfelipesantiago@gmail.com>
2025-09-29 10:07:42 -0300

    exercicios

--- Entrada-saida-dados/excecoes.py
+++ Entrada-saida-dados/excecoes.py
@@ -46,3 +46,16 @@ notas = open("file.txt", mode="r")
 content = notas.read()
 print(content.split())
 notas.close()
+
+recovery_students = []
+with open("file.txt") as grades_file:
+    for line in grades_file:
+        student_grade = line
+        student_grade = student_grade.split(" ")
+        if int(student_grade[1]) < 6:
+            recovery_students.append(student_grade[0] + "\n")
+
+
+with open("recoveryStudentsFile.txt", mode="w") as recovery_students_file:
+    print(recovery_students)
+    recovery_students_file.writelines(recovery_students)
