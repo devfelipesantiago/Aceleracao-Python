c6b97154a2afc7c91406aa1ee8bee397e32e1a15
Felipe Santiago <devfelipesantiago@gmail.com>
2025-10-28 10:53:31 -0300

    Correcoes

--- Flask/projeto-flask/src/app.py
+++ Flask/projeto-flask/src/app.py
@@ -2,9 +2,11 @@ from os import environ
 from flask import Flask
 from waitress import serve
 from controllers.jokes_controller import jokes_controller
+from controllers.music_controller import musics_controller
 
 app = Flask(__name__)
 app.register_blueprint(jokes_controller, url_prefix="/jokes")
+app.register_blueprint(musics_controller, url_prefix="/musics")
 
 
 def start_server(host: str = "0.0.0.0", port: int = 8000):
