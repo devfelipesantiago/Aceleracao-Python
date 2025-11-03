8cadf3245466ab9386a45fdebe303c1fb2a15094
Felipe Santiago <devfelipesantiago@gmail.com>
2025-11-03 10:26:08 -0300

    Limpa o db nos teste

--- Flask/python-conteudo-flask-project-manager/src/tests/conftest.py
+++ Flask/python-conteudo-flask-project-manager/src/tests/conftest.py
@@ -1,3 +1,4 @@
+from flask import Config
 import pytest
 from app import app
 
@@ -19,3 +20,21 @@ def response(client):
 # dos testes) para o nosso teste client que nos permite simular requisições à
 # aplicação e response que faz a requisição para a aplicação e nos fornece o
 # retorno dessa requisição.
+
+
+@pytest.fixture(autouse=True)
+def clear_database():
+    # Aqui você pode adicionar código para limpar ou resetar o banco de dados
+    # antes de cada teste, se necessário.
+    Config.db.mongo_db.drop()
+    yield
+    # Código para rodar após cada teste, se necessário.
+    Config.db.mongo_db.drop()
+
+
+# A fixture clear_database é marcada com autouse=True, o que significa que ela
+# será executada automaticamente antes e depois de cada teste, garantindo que
+# o banco de dados esteja limpo para cada teste.
+# Note que o código específico para limpar o banco de dados pode variar
+# dependendo do banco de dados que você está usando. Aqui, usamos um exemplo
+# genérico que pode ser adaptado conforme necessário.
