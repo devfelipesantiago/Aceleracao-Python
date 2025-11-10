b53cadf0c970f9237381c6c9622eb6f36176bb5d
Felipe Santiago <devfelipesantiago@gmail.com>
2025-11-10 11:31:09 -0300

    Renderizando formulários em templates

---
+++ Django/formularios/playlists/urls.py
@@ -0,0 +1,9 @@
+# playlists/urls.py
+
+from django.urls import path
+from playlists.views import music
+
+
+urlpatterns = [
+    path("musics/", music, name="musics-page"),
+]
--- Django/formularios/playlists/views.py
+++ Django/formularios/playlists/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# playlists/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from playlists.forms import CreateMusicForm
+
+
+def music(request):
+    form = CreateMusicForm()
+    context = {"form": form}
+    return render(request, "music.html", context)
