2a0569ecf6ec84b5beb073fbb9a1a03be68a3122
Felipe Santiago <devfelipesantiago@gmail.com>
2025-11-10 10:01:36 -0300

    Usando o BD com templates

--- Django/eventManager/event_manager/events/views.py
+++ Django/eventManager/event_manager/events/views.py
@@ -1,6 +1,8 @@
+# events/views.py
+from events.models import Event
 from django.shortcuts import render
 
 
-# Create your views here.
 def index(request):
-    return render(request, "home.html")
+    context = {"company": "Trybe", "events": Event.objects.all()}
+    return render(request, "home.html", context)
